b19381b4afabc41981a64cae8b148c7537c40abb
HAMM3R67 <tcschauer@gmail.com>
2015-07-29 20:54:24 -0600

    added input and started on the quotesAdd function

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,10 @@
 var app = angular.module('quoteBook');
 app.controller('mainCtrl', function($scope, QuoteFactory){
 	$scope.quotes = QuoteFactory.getData();
+	$scope.quotesAdd = function(text, author){
+		var newQuote = {};
+		newQuote.text = text;
+		newQuote.author = author;
+		QuoteFactory.addData(newQuote);
+	}
 });
