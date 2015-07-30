d266e626d8b6fe2c0b7d0576a6fd8f518778e8a7
HAMM3R67 <tcschauer@gmail.com>
2015-07-30 16:22:41 -0600

    add quote works

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,10 +1,20 @@
 var app = angular.module('quoteBook');
 app.controller('mainCtrl', function($scope, QuoteFactory){
 	$scope.quotes = QuoteFactory.getData();
+	
 	$scope.quotesAdd = function(text, author){
 		var newQuote = {};
-		newQuote.text = text;
-		newQuote.author = author;
+		newQuote.text = $scope.textQuote;
+		newQuote.author = $scope.authorQuote;
 		QuoteFactory.addData(newQuote);
+	};
+	
+	$scope.quotesRemove = function(text){
+		var removeQuote = {};
+		for(var i = 0; i < QuoteFactory.getData; i++){
+			if(QuoteFactory.getData[i] === text){
+				QuoteFactory.removeData(text);
+			}
+		}
 	}
 });
